a1742bdff81be515a22b10fc8a287468a8b4d6a1
Tejgaon College Photography Club <creative.tcpc@gmail.com>
2025-11-28 16:03:41 +0000

    Use Google fonts Poppins

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Poppins', 'sans-serif'],
+        sans: ['Poppins', ...fontFamily.sans],
       },
       colors: {
         background: 'hsl(var(--background))',
