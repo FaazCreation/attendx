61fa3ecd8e95532bae179f676853956e0976cc0b
Tejgaon College Photography Club <creative.tcpc@gmail.com>
2025-11-28 08:56:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3614636699-7a8b7",
+  "appId": "1:584202052556:web:042c7da393c52efbc27369",
+  "apiKey": "AIzaSyAteZOZplb7LqrV0pnK-9X5yB-oXIRJF4Y",
+  "authDomain": "studio-3614636699-7a8b7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "584202052556"
+};
