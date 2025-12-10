0802d74c1b843334171e0a508b837631793b89e3
FaazCreation <fh7614@gmail.com>
2025-12-10 23:07:51 +0600

    Replace qrcode.react with react-qr-code for React 18

--- src/utils/qrCodeGenerator.js
+++ src/utils/qrCodeGenerator.js
@@ -1,4 +1,6 @@
-import QRCode from 'qrcode.react';
 import React from 'react';
+import QRCode from 'react-qr-code';
 
-export const QRCodeComponent = ({ value }) => <QRCode value={value} size={128} />;
+export const QRCodeComponent = ({ value }) => (
+  <QRCode value={value} size={128} />
+);
